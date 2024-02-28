7c0cdc94b2d9f7c6e4409c4bc1a22dc43cd7a48f
Nadine Siccion <nv.siccion@gmail.com>
2024-02-28 18:46:26 +0800

    Implement operation() onlick of equal button
    Calculator can now perform the basic operations
    on the given numbers and display the answer
    and take the answer as the firstOpperand of the
    next operation.
    
    May need to figure out what equals should do when
    active operation is null.

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 let inputNumbers = [];
 let activeOperation = null;
 const operationButtons = document.querySelectorAll('.operation')
-let firstNum = null
-, secondNum = null
+let firstOpperand = null
+, secondOpperand = null
 
 const inputScreenP = document.querySelector('.input-screen p');
 
+/** 
+ * display argument on inputScreenP.
+ * 
+ * parameters: 
+ * String inputNumbers - string to display. 
+ */
 let updateInputScreen = function(inputNumbers) {
-    console.log('running function');
     inputScreenP.textContent = inputNumbers;
 }
+
+
 
 
 // what each button in the button pad will do
@@ -19,6 +26,9 @@ buttonPad.addEventListener('click', event => {
     event.stopPropagation();
     className = event.target.classList[0];
     // Number buttons
+    if (activeOperation != null && firstOpperand == null) {
+        getSecondOperand(event)
+    }
     if (className == 'number') { 
         inputNumbers.push(event.target.textContent);
         let display = inputNumbers.join("")
@@ -26,7 +36,14 @@ buttonPad.addEventListener('click', event => {
         updateInputScreen(display);
     } else if (className == 'operation') {
         setOperation(event)
-        // FIXME: add operation logic here
+    } else if (firstOpperand != null && event.target.id == "equal") {
+        secondOpperand = getDisplayedNum()
+        firstOpperand = operate(firstOpperand, secondOpperand, activeOperation)
+        // TODO: reflect answer at display
+        inputNumbers = []
+        updateInputScreen(firstOpperand)
+        // TODO: unclick operation (set activeOperation to null and toggle active class)
+        
     }
 });
 
@@ -48,6 +65,9 @@ const divide = function(num1, num2) {
 }
 
 // take in number from the input screen as first number
+/** 
+ * return textContent of inputScreenP. 
+ */
 const getDisplayedNum = function() {
     let inputText;
     inputText = inputScreenP.textContent
@@ -56,12 +76,17 @@ const getDisplayedNum = function() {
 
 // toggle active operation
 
+/** 
+ * Evaluates which of the operation buttons has been pushed and sets it as the activeOperation.
+ * 
+ * parameters: 
+ * Event event - event that has clicked on an operation button. 
+ */
 const setOperation = function(event) {
     let buttonId = event.target.id;
     let target = event.target;
     firstNum = getDisplayedNum();
-    if (firstNum == null) {
-        // pass
+    if (Boolean(inputScreenP.textContent) == false) {
         console.log('no operand given')
     } else if (activeOperation == null) {
         target.classList.toggle('active');
@@ -78,12 +103,47 @@ const setOperation = function(event) {
         activeOperation = buttonId;
         console.log('active operation set to ' + activeOperation);
     }
-
 }
 
 // take in second number
+// will only work if there is an active operator set
+const getSecondOperand = function(event) {
+    // set firstOpeOpperand as displayed number
+    firstOpperand = getDisplayedNum();
+    // clear numbers on display
+    inputNumbers = [];
+    // input new number (automatically happens)
+    console.log('firstOpperand set to ' + firstOpperand)
+}
+
 
 // equal sign to perform operation and display the anser to the screen
 const operate = function (num1, num2, operation) {
-
+    let answer;
+    switch(operation) {
+        case 'add':
+          answer = add(num1, num2)
+          break;
+        case 'subtract':
+          answer = subtract(num1, num2)
+          break;
+        case 'multiply':
+          answer = multiply(num1, num2)
+          break;
+        case 'divide':
+          answer = divide(num1, num2)
+          break;
+        default:
+          break;
+      }
+    return answer
 }
+
+// test function button
+// const equalBtn = document.querySelector('#equal')
+// equalBtn.addEventListener('click', event => {
+//     console.log(inputNumbers)
+//     console.log(inputScreenP.textContent)
+//     console.log(inputNumbers==null)
+//     console.log(inputScreenP.textContent==null)
+// })
