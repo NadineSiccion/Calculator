6cfb1077b02c8ff56846546189770e865ea08cb3
Nadine Siccion <nv.siccion@gmail.com>
2024-02-28 14:51:59 +0800

    Add radio-button functionality to operations
    Added a global activeOperation variable to js
    to identify active operation, can only have
    one active operation at a time. Added "active"
    class to css to reflect this in the UI.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // allow inputs and reflect it in UI
 let inputNumbers = [];
+let activeOperation = null;
+const operationButtons = document.querySelectorAll('.operation')
+let firstNum = null
+, secondNum = null
 
 const inputScreenP = document.querySelector('.input-screen p');
 
@@ -8,28 +12,21 @@ let updateInputScreen = function(inputNumbers) {
     inputScreenP.textContent = inputNumbers;
 }
 
+
+// what each button in the button pad will do
 const buttonPad = document.querySelector('.button-pad');
 buttonPad.addEventListener('click', event => {
     event.stopPropagation();
     className = event.target.classList[0];
+    // Number buttons
     if (className == 'number') { 
         inputNumbers.push(event.target.textContent);
         let display = inputNumbers.join("")
         console.log(display) 
         updateInputScreen(display);
     } else if (className == 'operation') {
-        operation = event.target.textContent;
-        switch (operation) {
-            // WIP
-            case '+':
-                break;
-            case '-':
-                break;
-            case '/':
-                break;
-            case '*':
-                break;
-        }
+        setOperation(event)
+        // FIXME: add operation logic here
     }
 });
 
@@ -50,4 +47,43 @@ const divide = function(num1, num2) {
     return num1 / num2;
 }
 
-// take in number from the 
+// take in number from the input screen as first number
+const getDisplayedNum = function() {
+    let inputText;
+    inputText = inputScreenP.textContent
+    return Number(inputText)
+}
+
+// toggle active operation
+
+const setOperation = function(event) {
+    let buttonId = event.target.id;
+    let target = event.target;
+    firstNum = getDisplayedNum();
+    if (firstNum == null) {
+        // pass
+        console.log('no operand given')
+    } else if (activeOperation == null) {
+        target.classList.toggle('active');
+        activeOperation = buttonId;
+        console.log('active operation set to ' + activeOperation);
+    } else if (activeOperation == buttonId) {
+        target.classList.toggle('active');
+        activeOperation = null;
+        console.log('active operation set to ' + activeOperation);
+    } else {
+        let activeOperationButton = Array.from(operationButtons).filter(item=>(item.id == activeOperation));
+        activeOperationButton[0].classList.toggle('active');
+        target.classList.toggle('active');
+        activeOperation = buttonId;
+        console.log('active operation set to ' + activeOperation);
+    }
+
+}
+
+// take in second number
+
+// equal sign to perform operation and display the anser to the screen
+const operate = function (num1, num2, operation) {
+
+}
